a3865b752a232d6af08d846ae4a11c75fd9db15f
Juzix1 <58827642+Juzix1@users.noreply.github.com>
2023-12-27 13:25:07 +0100

    fixed git ignore

--- .next/static/chunks/pages/posts/_app-f82abb3c23ea0b3f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[180],{4113:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/posts/_app",function(){return u(5361)}])},5361:function(){}},function(n){n.O(0,[888,774,179],function(){return n(n.s=4113)}),_N_E=n.O()}]);
